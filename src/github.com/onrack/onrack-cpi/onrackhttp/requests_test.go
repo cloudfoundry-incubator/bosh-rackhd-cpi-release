872ab83bf567008732d02e26f11fd86bf044270b
Megan Murawski <megan.hyland@emc.com>
2015-09-17 14:54:42 -0400

    Add basic tests for onrack HTTP reqeuest handling.
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>

--- src/github.com/onrack/onrack-cpi/onrackhttp/requests_test.go
+++ src/github.com/onrack/onrack-cpi/onrackhttp/requests_test.go
@@ -65,4 +65,56 @@ var _ = Describe("Requests", func() {
 			Expect(resp.StatusCode).To(Equal(404))
 		})
 	})
+
+	Describe("Getting nodes", func() {
+		It("return expected nodes fields", func() {
+			apiServerIP := os.Getenv("ON_RACK_API_URI")
+			Expect(apiServerIP).ToNot(BeEmpty())
+			c := config.Cpi{ApiServer: apiServerIP}
+
+			nodes, err := onrackhttp.GetNodes(c)
+			Expect(err).ToNot(HaveOccurred())
+
+			resp, err := http.Get(fmt.Sprintf("http://%s:8080/api/common/nodes", c.ApiServer))
+			Expect(err).ToNot(HaveOccurred())
+
+			b, err := ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+
+			var rawNodes []onrackhttp.Node
+			err = json.Unmarshal(b, &rawNodes)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(nodes).To(Equal(rawNodes))
+		})
+	})
+
+	Describe("Getting catalog", func() {
+		It("return ", func() {
+			apiServerIP := os.Getenv("ON_RACK_API_URI")
+			Expect(apiServerIP).ToNot(BeEmpty())
+			c := config.Cpi{ApiServer: apiServerIP}
+
+			nodes, err := onrackhttp.GetNodes(c)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(nodes).ToNot(BeEmpty())
+			testNode := nodes[0]
+
+			catalog, err := onrackhttp.GetNodeCatalog(c, testNode.ID)
+			Expect(err).ToNot(HaveOccurred())
+
+			resp, err := http.Get(fmt.Sprintf("http://%s:8080/api/common/nodes/%s/catalogs/ohai", c.ApiServer, testNode.ID))
+			Expect(err).ToNot(HaveOccurred())
+
+			b, err := ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+
+			var rawCatalog onrackhttp.NodeCatalog
+			err = json.Unmarshal(b, &rawCatalog)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(catalog).To(Equal(rawCatalog))
+		})
+	})
 })
