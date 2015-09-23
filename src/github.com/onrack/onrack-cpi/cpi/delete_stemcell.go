b2e60a1bf1f409be4074b880541a31af7c6bbbc9
Xuebin He <xuebin.he@emc.com>
2015-09-23 10:31:33 -0400

    Add DeleteStemcell
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>

---
+++ src/github.com/onrack/onrack-cpi/cpi/delete_stemcell.go
@@ -0,0 +1,24 @@
+package cpi
+
+import (
+	"errors"
+	"log"
+	"reflect"
+
+	"github.com/onrack/onrack-cpi/bosh"
+	"github.com/onrack/onrack-cpi/config"
+	"github.com/onrack/onrack-cpi/onrackhttp"
+)
+
+func DeleteStemcell(c config.Cpi, extInput bosh.ExternalInput) error {
+	var cid string
+
+	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(cid) {
+		log.Println("Received unexpected type for stemcell cid")
+		return errors.New("Received unexpected type for stemcell cid")
+	}
+
+	cid = extInput[0].(string)
+
+	return onrackhttp.DeleteFile(c, cid)
+}
