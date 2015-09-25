d449548bfa026533dad519bfbea6e668d76a8a1d
Xuebin He <xuebin.he@emc.com>
2015-09-25 16:43:53 -0400

    Add missing files.
    something strange is happening with our .gitignore
    
    Signed-off-by: Caleb Miles <cmiles@pivotal.io>

---
+++ src/github.com/onrack/onrack-cpi/workflows/reserve_node_workflow.go
@@ -0,0 +1,140 @@
+package workflows
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/onrack/onrack-cpi/config"
+	"github.com/onrack/onrack-cpi/onrackhttp"
+)
+
+func RunReserveNodeWorkflow(c config.Cpi, nodeID string, workflowName string, options ReserveNodeWorkflowOptions) error {
+	req := onrackhttp.RunWorkflowRequestBody{
+		Name:    workflowName,
+		Options: map[string]interface{}{"defaults": options},
+	}
+
+	return onrackhttp.RunWorkflow(c, nodeID, req)
+}
+
+func PublishReserveNodeWorkflow(c config.Cpi, uuid string) (string, error) {
+	tasks, workflow, err := generateReserveNodeWorkflow(uuid)
+	if err != nil {
+		return "", err
+	}
+
+	for i := range tasks {
+		err = onrackhttp.PublishTask(c, tasks[i])
+		if err != nil {
+			return "", err
+		}
+	}
+
+	w := reserveNodeWorkflow{}
+	err = json.Unmarshal(workflow, &w)
+	if err != nil {
+		log.Printf("error umarshalling workflow: %s", err)
+		return "", err
+	}
+
+	err = onrackhttp.PublishWorkflow(c, workflow)
+	if err != nil {
+		return "", err
+	}
+
+	return w.Name, nil
+}
+
+func generateReserveNodeWorkflow(uuid string) ([][]byte, []byte, error) {
+	reserve := reserveNodeTask{}
+	err := json.Unmarshal(reserveNodeTaskTemplate, &reserve)
+	if err != nil {
+		log.Printf("error unmarshalling reserve node task template: %s\n", err)
+		return nil, nil, fmt.Errorf("error unmarshalling reserve node task template: %s\n", err)
+	}
+
+	reserve.Name = fmt.Sprintf("%s.%s", reserve.Name, uuid)
+	reserve.UnusedName = fmt.Sprintf("%s.%s", reserve.UnusedName, "UPLOADED_BY_ONRACK_CPI")
+
+	reserveBytes, err := json.Marshal(reserve)
+	if err != nil {
+		log.Printf("error marshalling reserve node task template: %s\n", err)
+		return nil, nil, fmt.Errorf("error reserve provision node task template: %s\n", err)
+	}
+
+	w := reserveNodeWorkflow{}
+	err = json.Unmarshal(reserveNodeWorkflowTemplate, &w)
+	if err != nil {
+		log.Printf("error unmarshalling reserve node workflow template: %s\n", err)
+		return nil, nil, fmt.Errorf("error unmarshalling reserve node workflow template: %s\n", err)
+	}
+
+	w.Name = fmt.Sprintf("%s.%s", w.Name, uuid)
+	w.UnusedName = fmt.Sprintf("%s.%s", w.UnusedName, "UPLOADED_BY_ONRACK_CPI")
+	w.Tasks[3].TaskName = fmt.Sprintf("%s.%s", w.Tasks[3].TaskName, uuid)
+
+	wBytes, err := json.Marshal(w)
+	if err != nil {
+		log.Printf("error marshalling reserve node workflow template: %s\n", err)
+		return nil, nil, fmt.Errorf("error marshalling reserve node workflow template: %s\n", err)
+	}
+
+	return [][]byte{reserveBytes}, wBytes, nil
+}
+
+type ReserveNodeWorkflowOptions struct {
+	UUID *string `json:"uuid"`
+}
+
+type reserveNodeWorkflowOptionsContainer struct {
+	Options reserveNodeWorkflowDefaultOptionsContainer `json:"options"`
+}
+
+type reserveNodeWorkflowDefaultOptionsContainer struct {
+	Defaults ReserveNodeWorkflowOptions `json:"defaults"`
+}
+
+type reserveNodeWorkflow struct {
+	*onrackhttp.WorkflowStub
+	*reserveNodeWorkflowOptionsContainer
+	Tasks []onrackhttp.WorkflowTask `json:"tasks"`
+}
+
+var reserveNodeWorkflowTemplate = []byte(`{
+  "friendlyName": "BOSH Reserve Node",
+  "injectableName": "Graph.BOSH.ReserveNode",
+  "options": {
+    "defaults": {
+      "uuid": null
+    }
+  },
+  "tasks": [
+    {
+      "label": "set-boot-pxe",
+      "taskName": "Task.Obm.Node.PxeBoot",
+      "ignoreFailure": true
+    },
+    {
+      "label": "reboot",
+      "taskName": "Task.Obm.Node.Reboot",
+      "waitOn": {
+        "set-boot-pxe": "finished"
+      }
+    },
+    {
+      "label": "bootstrap-ubuntu",
+      "taskName": "Task.Linux.Bootstrap.Ubuntu",
+      "waitOn": {
+        "reboot": "succeeded"
+      }
+    },
+    {
+      "label": "reserve-node",
+      "taskName": "Task.BOSH.Reserve.Node",
+      "waitOn": {
+        "bootstrap-ubuntu": "succeeded"
+      }
+    }
+  ]
+}`)
