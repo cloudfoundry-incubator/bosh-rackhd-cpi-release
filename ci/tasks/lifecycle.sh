a5567944c158f852d9f89b6cc6a0c11f2631ee65
Megan Murawski <megan.hyland@emc.com>
2015-10-12 14:35:15 -0400

    - Better annotate lifeycle tests + Moved heredocs to the top + Added comments and echo as to which method being tested
    [#105332276](https://www.pivotaltracker.com/story/show/105332276)
    
    Signed-off-by: Thinh Nguyen <thinh.nguyen@emc.com>

--- ci/tasks/lifecycle.sh
+++ ci/tasks/lifecycle.sh
@@ -31,6 +31,8 @@ pushd ${PWD}/bosh-cpi-release/
 source .envrc
 go build github.com/onrack/onrack-cpi/onrack-cpi
 
+# Prepare config file
+echo "Prepare config file"
 cat > config_file <<EOF
 {
   "apiserver": "${ON_RACK_API_URI}",
@@ -48,20 +50,8 @@ EOF
 cat config_file
 config_path=${PWD}/config_file
 
-cat > create_stemcell_request <<EOF
-{"method": "create_stemcell", "arguments": ["${stemcell_path}"]}
-EOF
-cat create_stemcell_request
-
-response=$(cat create_stemcell_request | ./onrack-cpi -configPath=${config_path})
-echo ${response}
-stemcell_id=$(echo ${response} | jq .result)
-if [ -z "${stemcell_id}" ] || [ ${stemcell_id} == null ]; then
-  echo "can not retrieve stemcell id"
-  exit 1
-fi
-echo "got stemcell id: ${stemcell_id}"
-
+# Prepare bosh network configuration
+echo -e "\nPrepare bosh network configuration. Bosh network is"
 cat > bosh_networks <<EOF
 {
   "default": {
@@ -80,12 +70,34 @@ cat > bosh_networks <<EOF
 EOF
 cat bosh_networks
 
+# Prepare create stemcell request
+echo -e "\nPrepare create stemcell request. Request is"
+cat > create_stemcell_request <<EOF
+{"method": "create_stemcell", "arguments": ["${stemcell_path}"]}
+EOF
+cat create_stemcell_request
+
+# Run create stemcell method
+echo -e "\nRun create stemcell method"
+response=$(cat create_stemcell_request | ./onrack-cpi -configPath=${config_path})
+echo ${response}
+stemcell_id=$(echo ${response} | jq .result)
+if [ -z "${stemcell_id}" ] || [ ${stemcell_id} == null ]; then
+  echo "can not retrieve stemcell id"
+  exit 1
+fi
+echo "got stemcell id: ${stemcell_id}"
+
+# Prepare create vm request
+echo -e "\nPrepare create vm request"
 agent_id=$(uuidgen)
 cat > create_vm_request <<EOF
 {"method": "create_vm", "arguments": ["${agent_id}", ${stemcell_id}, {"public_key": "${AGENT_PUBLIC_KEY}"}, $(cat bosh_networks)]}
 EOF
 cat create_vm_request
 
+# Run create vm method
+echo -e "\nRun create vm method"
 vm_cid=$(cat create_vm_request | ./onrack-cpi -configPath=${config_path} | jq .result)
 if [ -z "${vm_cid}" ] || [ ${vm_cid} == null ]; then
   echo "can not retrieve vm cid"
@@ -93,14 +105,24 @@ if [ -z "${vm_cid}" ] || [ ${vm_cid} == null ]; then
 fi
 echo "got vm cid: ${vm_cid}"
 
+# Prepare delete vm request
+echo -e "\nPrepare delete vm request"
 cat > delete_vm_request <<EOF
 {"method": "delete_vm", "arguments": [${vm_cid}]}
 EOF
 cat delete_vm_request
+
+# Run delete vm method
+echo -e "\nRun delete vm method"
 cat delete_vm_request | ./onrack-cpi -configPath=${config_path} 2>&1
 
+# Prepare delete stemcell request
+echo -e "\nPrepare delete stemcell request"
 cat > delete_stemcell_request <<EOF
 {"method": "delete_stemcell", "arguments": [${stemcell_id}]}
 EOF
 cat delete_stemcell_request
+
+# Run delete stemcell method
+echo -e "\nRun delete stemcell method"
 cat delete_stemcell_request | ./onrack-cpi -configPath=${config_path}
