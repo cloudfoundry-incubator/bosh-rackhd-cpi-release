e9820479cdcddde051b35901ca64bc2370ca3b78
Victor Fong <victor.fong@emc.com>
2015-11-17 15:40:49 -0500

    Add stage to create final release, tag version, and upload release
    [#102600734](https://www.pivotaltracker.com/story/show/102600734)
    
    Signed-off-by: Tyson McNulty <tmcnulty@pivotal.io>

---
+++ ci/tasks/promote_candidate.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+
+source bosh-cpi-release/ci/tasks/utils.sh
+
+check_param S3_ACCESS_KEY_ID
+check_param S3_SECRET_ACCESS_KEY
+
+# Creates an integer version number from the semantic version format
+# May be changed when we decide to fully use semantic versions for releases
+integer_version=`cut -d "." -f1 release-version-semver/number`
+echo $integer_version > integer_version
+
+cd bosh-cpi-release
+
+source /etc/profile.d/chruby.sh
+chruby 2.1.2
+
+set +x
+echo creating config/private.yml with blobstore secrets
+cat > config/private.yml << EOF
+---
+blobstore:
+  s3:
+    access_key_id: ${S3_ACCESS_KEY_ID}
+    secret_access_key: ${S3_SECRET_ACCESS_KEY}
+EOF
+set -x
+
+echo "using bosh CLI version..."
+bosh version
+
+echo "finalizing CPI release..."
+bosh create release --final --force --with-tarball
+bosh finalize release ../bosh-cpi-dev-artifacts/*.tgz --version $integer_version
+
+rm config/private.yml
+
+git diff | cat
+git add .
+
+git config --global user.email emccmd-eng@emc.com
+git config --global user.name EMCCMD-CI
+git commit -m ":airplane: New final release v $integer_version" -m "[ci skip]"
