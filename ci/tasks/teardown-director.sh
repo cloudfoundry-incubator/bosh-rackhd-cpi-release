6e4a2369c157509ba6aee6eeb202d881655fb850
Tyson McNulty <tmcnulty@pivotal.io>
2015-11-17 14:56:00 -0500

    Externalize bat deployment and bat release names to make multiple workstation execution possible.
    Signed-off-by: Victor Fong <victor.fong@emc.com>

--- ci/tasks/teardown-director.sh
+++ ci/tasks/teardown-director.sh
@@ -5,9 +5,11 @@ set -e -x
 source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param BOSH_VSPHERE_DIRECTOR
+check_param DIRECTOR_DEPLOYMENT_NAME
+check_param CPI_RELEASE_NAME
 
 bosh -n target ${BOSH_VSPHERE_DIRECTOR}
-echo 'yes' | bosh --user admin --password admin delete deployment bat-director
+echo 'yes' | bosh --user admin --password admin delete deployment ${DIRECTOR_DEPLOYMENT_NAME}
 
 echo "Remove Release"
-echo 'yes' | bosh --user admin --password admin delete release bosh-rackhd-cpi
+echo 'yes' | bosh --user admin --password admin delete release ${CPI_RELEASE_NAME}
