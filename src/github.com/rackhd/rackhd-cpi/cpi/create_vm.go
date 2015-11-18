af1216c585e60ff57465bf3f487f934eb953f5e5
Avery Yen <ayen@pivotal.io>
2015-11-18 11:43:20 -0500

    Revert "Remove blocking code in createvm to pass pipeline"
    This reverts commit e70f80110670fe580b1993207218730da3cfc136.

--- src/github.com/rackhd/rackhd-cpi/cpi/create_vm.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/create_vm.go
@@ -23,12 +23,7 @@ func CreateVM(c config.Cpi, extInput bosh.MethodArguments) (string, error) {
 		return "", err
 	}
 
-	nodeID, err := tryReservation(
-		c,
-		agentID,
-		func(config.Cpi) error { return nil }, 
-		selectNodeFromRackHD,
-		reserveNodeFromRackHD)
+	nodeID, err := tryReservation(c, agentID, blockNodesWithoutEphemeralDisk, selectNodeFromRackHD, reserveNodeFromRackHD)
 	if err != nil {
 		return "", err
 	}
