877c6da2f6476217d6fe88b5d1bb85dff608b45e
Avery Yen <ayen@pivotal.io>
2016-01-11 11:11:49 -0500

    Add detach_disk
    [#110997556](https://www.pivotaltracker.com/story/show/110997556)
    
    Signed-off-by: Victor Fong <victor.fong@emc.com>

---
+++ src/github.com/rackhd/rackhd-cpi/cpi/detach_disk.go
@@ -0,0 +1,57 @@
+package cpi
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/rackhd/rackhd-cpi/bosh"
+	"github.com/rackhd/rackhd-cpi/config"
+	"github.com/rackhd/rackhd-cpi/rackhdapi"
+)
+
+func DetachDisk(c config.Cpi, extInput bosh.MethodArguments) error {
+	var vmCID string
+	var diskCID string
+
+	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(vmCID) {
+		return errors.New("Received unexpected type for vm cid")
+	}
+
+	if reflect.TypeOf(extInput[1]) != reflect.TypeOf(diskCID) {
+		return errors.New("Received unexpected type for disk cid")
+	}
+
+	vmCID = extInput[0].(string)
+	diskCID = extInput[1].(string)
+
+	nodes, err := rackhdapi.GetNodes(c)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range nodes {
+		if node.CPI.PersistentDisk.DiskCID == diskCID {
+			if !node.CPI.PersistentDisk.IsAttached {
+				return fmt.Errorf("Disk: %s is detached\n", diskCID)
+			}
+
+			if node.CPI.VMCID != vmCID {
+				return fmt.Errorf("Disk %s does not belong to VM %s\n", diskCID, vmCID)
+			}
+
+			node.CPI.PersistentDisk.IsAttached = false
+			body := node.CPI
+			bodyBytes, err := json.Marshal(body)
+			if err != nil {
+				return err
+			}
+
+			rackhdapi.PatchNode(c, node.ID, bodyBytes)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("Disk: %s not found\n", diskCID)
+}
