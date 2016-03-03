baf13a0dfde5bdcae1510e2282b3849259958147
Xuebin He <xuebin.he@emc.com>
2016-03-03 15:57:03 -0500

    Add output in promote-candidate due to updated concourse
    Signed-off-by: Victor Fong <victor.fong@emc.com>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -2,21 +2,19 @@
 
 set -e -x
 
-
 source bosh-cpi-release/ci/tasks/utils.sh
 
 check_param S3_ACCESS_KEY_ID
 check_param S3_SECRET_ACCESS_KEY
 
+
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
 integer_version=`cut -d "." -f1 release-version-semver/number`
-mkdir -p promote
-echo $integer_version > promote/integer_version
+echo ${integer_version} > promote/integer_version
 
 cp -r bosh-cpi-release promote/bosh-cpi-release
-cd promote/bosh-cpi-release
-
+pushd promote/bosh-cpi-release
 #source /etc/profile.d/chruby.sh
 #chruby 2.1.2
 
@@ -36,8 +34,8 @@ echo "using bosh CLI version..."
 bosh version
 
 echo "finalizing CPI release..."
-echo '' | bosh create release --force --with-tarball --version $integer_version
-bosh finalize release dev_releases/bosh-rackhd-cpi/*.tgz --version $integer_version
+echo '' | bosh create release --force --with-tarball --version ${integer_version}
+bosh finalize release dev_releases/bosh-rackhd-cpi/*.tgz --version ${integer_version}
 
 rm config/private.yml
 
@@ -46,4 +44,5 @@ git add .
 
 git config --global user.email emccmd-eng@emc.com
 git config --global user.name EMCCMD-CI
-git commit -m ":airplane: New final release v $integer_version" -m "[ci skip]"
+git commit -m ":airplane: New final release v ${integer_version}" -m "[ci skip]"
+popd
