05538e3d4192e7d083642fa171a266ab264955f1
Victor Fong <victor.fong@emc.com>
2016-03-03 15:12:23 -0500

    Fix typo
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- ci/tasks/promote-candidate.sh
+++ ci/tasks/promote-candidate.sh
@@ -10,9 +10,9 @@ check_param S3_SECRET_ACCESS_KEY
 # Creates an integer version number from the semantic version format
 # May be changed when we decide to fully use semantic versions for releases
 integer_version=`cut -d "." -f1 release-version-semver/number`
-pushd ${integer_version} > integer_version
+echo ${integer_version} > integer_version
 
-cd bosh-cpi-release
+pushd bosh-cpi-release
 
 #source /etc/profile.d/chruby.sh
 #chruby 2.1.2
