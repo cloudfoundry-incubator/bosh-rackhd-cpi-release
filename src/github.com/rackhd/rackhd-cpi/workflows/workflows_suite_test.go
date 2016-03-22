b12e1856ee3a8323e4a1f4cd64a39c61c9e0a991
Thinh Nguyen <thinh.nguyen@emc.com>
2016-03-22 14:46:38 -0400

    Discard logs in workflow test suite
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- src/github.com/rackhd/rackhd-cpi/workflows/workflows_suite_test.go
+++ src/github.com/rackhd/rackhd-cpi/workflows/workflows_suite_test.go
@@ -1,6 +1,10 @@
 package workflows_test
 
 import (
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -8,6 +12,8 @@ import (
 )
 
 func TestWorkflows(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Workflows Suite")
 }
