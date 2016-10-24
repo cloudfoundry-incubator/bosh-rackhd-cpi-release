90492b3a829fe35586440460b9521c41ed0f17a0
Megan Murawski <megan.hyland@emc.com>
2016-10-24 15:18:47 -0400

    Remove unused code from node
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- src/github.com/rackhd/rackhd-cpi/rackhdapi/nodes.go
+++ src/github.com/rackhd/rackhd-cpi/rackhdapi/nodes.go
@@ -1,204 +1,170 @@
 package rackhdapi
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+  "bytes"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "io/ioutil"
+  "net/http"
 
-	"github.com/rackhd/rackhd-cpi/config"
-	"github.com/rackhd/rackhd-cpi/models"
+  "github.com/rackhd/rackhd-cpi/config"
+  "github.com/rackhd/rackhd-cpi/helpers"
+  "github.com/rackhd/rackhd-cpi/models"
 )
 
+// GetNodes returns all nodes
 func GetNodes(c config.Cpi) ([]models.Node, error) {
-	nodesURL := fmt.Sprintf("%s/api/1.1/nodes", c.ApiServer)
-	resp, err := http.Get(nodesURL)
-	if err != nil {
-		return []models.Node{}, fmt.Errorf("error fetching nodes %s", err)
-	}
-	defer resp.Body.Close()
+  url := fmt.Sprintf("%s/api/2.0/nodes", c.ApiServer)
+  respBody, err := helpers.MakeRequest(url, "GET", 200, nil)
+  if err != nil {
+    return []models.Node{}, fmt.Errorf("error getting nodes: %s", err)
+  }
 
-	if resp.StatusCode != 200 {
-		return []models.Node{}, fmt.Errorf("Failed getting nodes with status: %s, err: %s", resp.Status, err)
-	}
+  var nodes []models.Node
+  err = json.Unmarshal(respBody, &nodes)
+  if err != nil {
+    return []models.Node{}, fmt.Errorf("error unmarshalling /common/nodes response %s", err)
+  }
 
-	nodeBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return []models.Node{}, fmt.Errorf("error reading node response body %s", err)
-	}
-
-	var nodes []models.Node
-	err = json.Unmarshal(nodeBytes, &nodes)
-	if err != nil {
-		fmt.Printf("body: %+v", string(nodeBytes))
-		return []models.Node{}, fmt.Errorf("error unmarshalling /common/nodes response %s", err)
-	}
-
-	return nodes, nil
-}
-
-func GetNodeByVMCID(c config.Cpi, cid string) (models.Node, error) {
-	nodes, err := GetNodes(c)
-	if err != nil {
-		return models.Node{}, err
-	}
-
-	for _, node := range nodes {
-		if node.CID == cid {
-			return node, nil
-		}
-	}
-
-	return models.Node{}, fmt.Errorf("vm with cid: %s was not found", cid)
+  return nodes, nil
 }
 
 func GetNode(c config.Cpi, nodeID string) (models.Node, error) {
-	nodeURL := fmt.Sprintf("%s/api/1.1/nodes/%s", c.ApiServer, nodeID)
-	resp, err := http.Get(nodeURL)
-	if err != nil {
-		return models.Node{}, fmt.Errorf("error fetching node %s: %s", nodeID, err)
-	}
-	defer resp.Body.Close()
+  nodeURL := fmt.Sprintf("%s/api/2.0/nodes/%s", c.ApiServer, nodeID)
+  resp, err := http.Get(nodeURL)
+  if err != nil {
+    return models.Node{}, fmt.Errorf("error fetching node %s: %s", nodeID, err)
+  }
+  defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return models.Node{}, fmt.Errorf("Failed getting node %s with status: %s", nodeID, resp.Status)
-	}
+  if resp.StatusCode != 200 {
+    return models.Node{}, fmt.Errorf("Failed getting node %s with status: %s", nodeID, resp.Status)
+  }
 
-	nodeBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return models.Node{}, fmt.Errorf("error reading node %s response body %s", nodeID, err)
-	}
+  nodeBytes, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return models.Node{}, fmt.Errorf("error reading node %s response body %s", nodeID, err)
+  }
 
-	var node models.Node
-	err = json.Unmarshal(nodeBytes, &node)
-	if err != nil {
-		return models.Node{}, fmt.Errorf("error unmarshalling /common/node/%s response %s", nodeID, err)
-	}
+  var node models.Node
+  err = json.Unmarshal(nodeBytes, &node)
+  if err != nil {
+    return models.Node{}, fmt.Errorf("error unmarshalling /common/node/%s response %s", nodeID, err)
+  }
 
-	return node, nil
+  return node, nil
 }
 
 func GetOBMSettings(c config.Cpi, nodeID string) ([]models.OBM, error) {
-	nodeURL := fmt.Sprintf("%s/api/2.0/nodes/%s", c.ApiServer, nodeID)
-	resp, err := http.Get(nodeURL)
-	if err != nil {
-		return nil, fmt.Errorf("error getting node %s", err)
-	}
-	defer resp.Body.Close()
+  nodeURL := fmt.Sprintf("%s/api/2.0/nodes/%s", c.ApiServer, nodeID)
+  resp, err := http.Get(nodeURL)
+  if err != nil {
+    return nil, fmt.Errorf("error getting node %s", err)
+  }
+  defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Failed getting node with status: %s, err: %s", resp.Status, err)
-	}
+  if resp.StatusCode != 200 {
+    return nil, fmt.Errorf("Failed getting node with status: %s, err: %s", resp.Status, err)
+  }
 
-	b, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("obm body: %+v", string(b))
-	if err != nil {
-		return nil, fmt.Errorf("error reading node body %s", err)
-	}
+  b, err := ioutil.ReadAll(resp.Body)
+  fmt.Printf("obm body: %+v", string(b))
+  if err != nil {
+    return nil, fmt.Errorf("error reading node body %s", err)
+  }
 
-	var node models.Node
-	err = json.Unmarshal(b, &node)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshal node body %s", err)
-	}
+  var node models.Node
+  err = json.Unmarshal(b, &node)
+  if err != nil {
+    return nil, fmt.Errorf("error unmarshal node body %s", err)
+  }
 
-	if len(node.OBMS) == 0 {
-		return nil, errors.New("error: got empty obm settings")
-	}
+  if len(node.OBMS) == 0 {
+    return nil, errors.New("error: got empty obm settings")
+  }
 
-	return node.OBMS, nil
+  return node.OBMS, nil
 }
 
 func GetOBMServiceName(c config.Cpi, nodeID string) (string, error) {
-	obmSettings, err := GetOBMSettings(c, nodeID)
-	if err != nil {
-		return "", fmt.Errorf("error retrieving obm settings of node: %s, error: %v", nodeID, err)
-	}
+  obmSettings, err := GetOBMSettings(c, nodeID)
+  if err != nil {
+    return "", fmt.Errorf("error retrieving obm settings of node: %s, error: %v", nodeID, err)
+  }
 
-	return obmSettings[0].ServiceName, nil
-}
-
-func ReleaseNode(c config.Cpi, nodeID string) error {
-	blockFlag := []byte(fmt.Sprintf("{\"status\": \"%s\"}", models.Available))
-	return PatchNode(c, nodeID, blockFlag)
+  return obmSettings[0].ServiceName, nil
 }
 
 func GetNodeCatalog(c config.Cpi, nodeID string) (models.NodeCatalog, error) {
-	catalogURL := fmt.Sprintf("%s/api/2.0/nodes/%s/catalogs/ohai", c.ApiServer, nodeID)
-	resp, err := http.Get(catalogURL)
-	if err != nil {
-		return models.NodeCatalog{}, fmt.Errorf("error getting catalog %s", err)
-	}
-	defer resp.Body.Close()
+  catalogURL := fmt.Sprintf("%s/api/2.0/nodes/%s/catalogs/ohai", c.ApiServer, nodeID)
+  resp, err := http.Get(catalogURL)
+  if err != nil {
+    return models.NodeCatalog{}, fmt.Errorf("error getting catalog %s", err)
+  }
+  defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return models.NodeCatalog{}, fmt.Errorf("Failed getting node catalog with status: %s, err: %s", resp.Status, err)
-	}
+  if resp.StatusCode != 200 {
+    return models.NodeCatalog{}, fmt.Errorf("Failed getting node catalog with status: %s, err: %s", resp.Status, err)
+  }
 
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return models.NodeCatalog{}, fmt.Errorf("error reading catalog body %s", err)
-	}
+  b, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return models.NodeCatalog{}, fmt.Errorf("error reading catalog body %s", err)
+  }
 
-	var nodeCatalog models.NodeCatalog
-	err = json.Unmarshal(b, &nodeCatalog)
-	if err != nil {
-		return models.NodeCatalog{}, fmt.Errorf("error unmarshal catalog body %s", err)
-	}
+  var nodeCatalog models.NodeCatalog
+  err = json.Unmarshal(b, &nodeCatalog)
+  if err != nil {
+    return models.NodeCatalog{}, fmt.Errorf("error unmarshal catalog body %s", err)
+  }
 
-	return nodeCatalog, nil
-}
-
-func BlockNode(c config.Cpi, nodeID string) error {
-	blockFlag := []byte(fmt.Sprintf("{\"status\": \"%s\", \"status_reason\": \"%s\"}", models.Blocked, models.DiskReason))
-	return PatchNode(c, nodeID, blockFlag)
+  return nodeCatalog, nil
 }
 
 func SetNodeMetadata(c config.Cpi, nodeID string, metadata string) error {
-	metadataBytes := []byte(fmt.Sprintf("{\"metadata\": %s}", metadata))
-	return PatchNode(c, nodeID, metadataBytes)
+  metadataBytes := []byte(fmt.Sprintf("{\"metadata\": %s}", metadata))
+  return PatchNode(c, nodeID, metadataBytes)
 }
 
 func PatchNode(c config.Cpi, nodeID string, body []byte) error {
-	url := fmt.Sprintf("%s/api/2.0/nodes/%s", c.ApiServer, nodeID)
+  url := fmt.Sprintf("%s/api/2.0/nodes/%s", c.ApiServer, nodeID)
 
-	request, err := http.NewRequest("PATCH", url, bytes.NewReader(body))
-	if err != nil {
-		return fmt.Errorf("Error building request to api server: %s", err)
-	}
+  request, err := http.NewRequest("PATCH", url, bytes.NewReader(body))
+  if err != nil {
+    return fmt.Errorf("Error building request to api server: %s", err)
+  }
 
-	request.Header.Set("Content-Type", "application/json")
-	request.ContentLength = int64(len(body))
+  request.Header.Set("Content-Type", "application/json")
+  request.ContentLength = int64(len(body))
 
-	resp, err := http.DefaultClient.Do(request)
-	if err != nil {
-		return fmt.Errorf("Error making request to api server: %s", err)
-	}
+  resp, err := http.DefaultClient.Do(request)
+  if err != nil {
+    return fmt.Errorf("Error making request to api server: %s", err)
+  }
 
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("Failed patching URL: %s with status: %s", url, resp.Status)
-	}
+  if resp.StatusCode != 200 {
+    return fmt.Errorf("Failed patching URL: %s with status: %s", url, resp.Status)
+  }
 
-	return nil
+  return nil
 }
 
 func MakeDiskRequest(c config.Cpi, node models.Node, newDiskState bool) error {
-	container := models.PersistentDiskSettingsContainer{
-		PersistentDisk: node.PersistentDisk,
-	}
-	container.PersistentDisk.IsAttached = newDiskState
+  container := models.PersistentDiskSettingsContainer{
+    PersistentDisk: node.PersistentDisk,
+  }
+  container.PersistentDisk.IsAttached = newDiskState
 
-	bodyBytes, err := json.Marshal(container)
-	if err != nil {
-		return err
-	}
+  bodyBytes, err := json.Marshal(container)
+  if err != nil {
+    return err
+  }
 
-	err = PatchNode(c, node.ID, bodyBytes)
-	if err != nil {
-		return fmt.Errorf("Error requesting new disk state: %v", err)
-	}
+  err = PatchNode(c, node.ID, bodyBytes)
+  if err != nil {
+    return fmt.Errorf("Error requesting new disk state: %v", err)
+  }
 
-	return nil
+  return nil
 }
--- src/github.com/rackhd/rackhd-cpi/rackhdapi/nodes_test.go
+++ src/github.com/rackhd/rackhd-cpi/rackhdapi/nodes_test.go
@@ -1,190 +1,154 @@
 package rackhdapi_test
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "net/http"
 
-	"github.com/rackhd/rackhd-cpi/config"
-	"github.com/rackhd/rackhd-cpi/helpers"
-	"github.com/rackhd/rackhd-cpi/models"
-	"github.com/rackhd/rackhd-cpi/rackhdapi"
+  "github.com/rackhd/rackhd-cpi/config"
+  "github.com/rackhd/rackhd-cpi/helpers"
+  "github.com/rackhd/rackhd-cpi/models"
+  "github.com/rackhd/rackhd-cpi/rackhdapi"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/ghttp"
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  "github.com/onsi/gomega/ghttp"
 )
 
 var _ = Describe("Nodes", func() {
-	var server *ghttp.Server
-	var jsonReader *strings.Reader
-	var cpiConfig config.Cpi
+  var server *ghttp.Server
+  var cpiConfig config.Cpi
 
-	BeforeEach(func() {
-		server, jsonReader, cpiConfig, _ = helpers.SetUp("")
-	})
+  BeforeEach(func() {
+    server, _, cpiConfig, _ = helpers.SetUp("")
+  })
 
-	AfterEach(func() {
-		server.Close()
-	})
+  AfterEach(func() {
+    server.Close()
+  })
 
-	Describe("ReleaseNode", func() {
-		It("return a node with reserved flag unset", func() {
-			apiServer, err := helpers.GetRackHDHost()
-			Expect(err).ToNot(HaveOccurred())
+  Describe("ReleaseNode", func() {
+    It("return a node with reserved flag unset", func() {
+      apiServer, err := helpers.GetRackHDHost()
+      Expect(err).ToNot(HaveOccurred())
 
-			c := config.Cpi{ApiServer: apiServer}
+      c := config.Cpi{ApiServer: apiServer}
 
-			nodes, err := rackhdapi.GetNodes(c)
-			Expect(err).ToNot(HaveOccurred())
-			targetNodeID := nodes[0].ID
-			err = rackhdapi.ReleaseNode(c, targetNodeID)
-			Expect(err).ToNot(HaveOccurred())
-			nodeURL := fmt.Sprintf("%s/api/1.1/nodes/%s", c.ApiServer, targetNodeID)
+      nodes, err := rackhdapi.GetNodes(c)
+      Expect(err).ToNot(HaveOccurred())
+      targetNodeID := nodes[0].ID
+      err = rackhdapi.ReleaseNode(c, targetNodeID)
+      Expect(err).ToNot(HaveOccurred())
+      nodeURL := fmt.Sprintf("%s/api/1.1/nodes/%s", c.ApiServer, targetNodeID)
 
-			resp, err := http.Get(nodeURL)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(resp.StatusCode).To(Equal(200))
+      resp, err := http.Get(nodeURL)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(resp.StatusCode).To(Equal(200))
 
-			nodeBytes, err := ioutil.ReadAll(resp.Body)
-			Expect(err).ToNot(HaveOccurred())
+      nodeBytes, err := ioutil.ReadAll(resp.Body)
+      Expect(err).ToNot(HaveOccurred())
 
-			var node models.Node
-			err = json.Unmarshal(nodeBytes, &node)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(node.Status).To(Equal(models.Available))
-		})
-	})
+      var node models.Node
+      err = json.Unmarshal(nodeBytes, &node)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(node.Status).To(Equal(models.Available))
+    })
+  })
 
-	Describe("Getting nodes", func() {
-		It("return expected nodes' fields", func() {
-			expectedNodes := helpers.LoadNodes("../spec_assets/dummy_two_node_response.json")
-			expectedNodesData, err := json.Marshal(expectedNodes)
-			Expect(err).ToNot(HaveOccurred())
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/1.1/nodes"),
-					ghttp.RespondWith(http.StatusOK, expectedNodesData),
-				),
-			)
+  Describe("Getting nodes", func() {
+    It("return expected nodes' fields", func() {
+      expectedNodes := helpers.LoadNodes("../spec_assets/dummy_two_node_response.json")
+      expectedNodesData, err := json.Marshal(expectedNodes)
+      Expect(err).ToNot(HaveOccurred())
+      server.AppendHandlers(
+        ghttp.CombineHandlers(
+          ghttp.VerifyRequest("GET", "/api/1.1/nodes"),
+          ghttp.RespondWith(http.StatusOK, expectedNodesData),
+        ),
+      )
 
-			nodes, err := rackhdapi.GetNodes(cpiConfig)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(server.ReceivedRequests()).To(HaveLen(1))
-			Expect(nodes).To(Equal(expectedNodes))
-		})
-	})
+      nodes, err := rackhdapi.GetNodes(cpiConfig)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(server.ReceivedRequests()).To(HaveLen(1))
+      Expect(nodes).To(Equal(expectedNodes))
+    })
+  })
 
-	Describe("Getting a single node by CID", func() {
-		It("returns expected node's fields", func() {
-			expectedNodes := helpers.LoadNodes("../spec_assets/dummy_all_nodes_are_vms.json")
-			expectedNodesData, err := json.Marshal(expectedNodes)
-			Expect(err).ToNot(HaveOccurred())
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/1.1/nodes"),
-					ghttp.RespondWith(http.StatusOK, expectedNodesData),
-				),
-			)
+  Describe("Getting a single node by nodeID", func() {
+    It("returns node with the nodeID specified", func() {
+      expectedNode := helpers.LoadNode("../spec_assets/dummy_create_vm_with_disk_response.json")
+      expectedNodeData, err := json.Marshal(expectedNode)
+      Expect(err).ToNot(HaveOccurred())
+      server.AppendHandlers(
+        ghttp.CombineHandlers(
+          ghttp.VerifyRequest("GET", "/api/1.1/nodes/5665a65a0561790005b77b85"),
+          ghttp.RespondWith(http.StatusOK, expectedNodeData),
+        ),
+      )
 
-			node, err := rackhdapi.GetNodeByVMCID(cpiConfig, "vm-5678")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(node).To(Equal(expectedNodes[0]))
-		})
-	})
+      node, err := rackhdapi.GetNode(cpiConfig, "5665a65a0561790005b77b85")
+      Expect(err).ToNot(HaveOccurred())
+      Expect(node).To(Equal(expectedNode))
+    })
+  })
 
-	Describe("Getting a single node by nodeID", func() {
-		It("returns node with the nodeID specified", func() {
-			expectedNode := helpers.LoadNode("../spec_assets/dummy_create_vm_with_disk_response.json")
-			expectedNodeData, err := json.Marshal(expectedNode)
-			Expect(err).ToNot(HaveOccurred())
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/1.1/nodes/5665a65a0561790005b77b85"),
-					ghttp.RespondWith(http.StatusOK, expectedNodeData),
-				),
-			)
+  Describe("GetOBMSettings", func() {
+    It("returns a node's OBM settings", func() {
+      dummyResponsePath := "../spec_assets/dummy_one_node_response.json"
+      httpResponse := helpers.LoadJSON(dummyResponsePath)
+      expectedResponse := helpers.LoadNode(dummyResponsePath)
 
-			node, err := rackhdapi.GetNode(cpiConfig, "5665a65a0561790005b77b85")
-			Expect(err).ToNot(HaveOccurred())
-			Expect(node).To(Equal(expectedNode))
-		})
-	})
+      nodeID := "nodeID"
+      server.AppendHandlers(
+        ghttp.CombineHandlers(
+          ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
+          ghttp.RespondWith(http.StatusOK, httpResponse),
+        ),
+      )
 
-	Describe("GetOBMSettings", func() {
-		It("returns a node's OBM settings", func() {
-			dummyResponsePath := "../spec_assets/dummy_one_node_response.json"
-			httpResponse := helpers.LoadJSON(dummyResponsePath)
-			expectedResponse := helpers.LoadNode(dummyResponsePath)
+      response, err := rackhdapi.GetOBMSettings(cpiConfig, nodeID)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(server.ReceivedRequests()).To(HaveLen(1))
+      Expect(response).To(Equal(expectedResponse.OBMS))
+    })
+  })
 
-			nodeID := "nodeID"
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
-					ghttp.RespondWith(http.StatusOK, httpResponse),
-				),
-			)
+  Describe("Getting catalog", func() {
+    It("returns a catalog", func() {
+      expectedNodeCatalog := helpers.LoadNodeCatalog("../spec_assets/dummy_node_catalog_response.json")
+      expectedNodeCatalogData, err := json.Marshal(expectedNodeCatalog)
+      testNodeID := "55e79eb14e66816f6152fffb"
+      Expect(err).ToNot(HaveOccurred())
+      server.AppendHandlers(
+        ghttp.CombineHandlers(
+          ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/nodes/%s/catalogs/ohai", testNodeID)),
+          ghttp.RespondWith(http.StatusOK, expectedNodeCatalogData),
+        ),
+      )
 
-			response, err := rackhdapi.GetOBMSettings(cpiConfig, nodeID)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(server.ReceivedRequests()).To(HaveLen(1))
-			Expect(response).To(Equal(expectedResponse.OBMS))
-		})
-	})
+      catalog, err := rackhdapi.GetNodeCatalog(cpiConfig, testNodeID)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(server.ReceivedRequests()).To(HaveLen(1))
+      Expect(catalog).To(Equal(expectedNodeCatalog))
+    })
+  })
 
-	Describe("Getting catalog", func() {
-		It("returns a catalog", func() {
-			expectedNodeCatalog := helpers.LoadNodeCatalog("../spec_assets/dummy_node_catalog_response.json")
-			expectedNodeCatalogData, err := json.Marshal(expectedNodeCatalog)
-			testNodeID := "55e79eb14e66816f6152fffb"
-			Expect(err).ToNot(HaveOccurred())
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/nodes/%s/catalogs/ohai", testNodeID)),
-					ghttp.RespondWith(http.StatusOK, expectedNodeCatalogData),
-				),
-			)
+  Describe("Setting node metadata", func() {
+    It("Adds metadata to the node", func() {
+      nodeID := "node_id"
+      metadata := "{}"
 
-			catalog, err := rackhdapi.GetNodeCatalog(cpiConfig, testNodeID)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(server.ReceivedRequests()).To(HaveLen(1))
-			Expect(catalog).To(Equal(expectedNodeCatalog))
-		})
-	})
+      server.AppendHandlers(
+        ghttp.CombineHandlers(
+          ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
+          ghttp.VerifyJSON(fmt.Sprintf(`{"metadata": %s}`, metadata)),
+        ),
+      )
 
-	Describe("blocking nodes", func() {
-		It("sends a request to block a node", func() {
-			nodes := helpers.LoadNodes("../spec_assets/dummy_two_node_response.json")
-
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", nodes[0].ID)),
-					ghttp.VerifyJSON(fmt.Sprintf(`{"status": "%s", "status_reason": "%s"}`, "blocked", "Node has missing disks")),
-				),
-			)
-
-			rackhdapi.BlockNode(cpiConfig, nodes[0].ID)
-			Expect(server.ReceivedRequests()).To(HaveLen(1))
-		})
-	})
-
-	Describe("Setting node metadata", func() {
-		It("Adds metadata to the node", func() {
-			nodeID := "node_id"
-			metadata := "{}"
-
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
-					ghttp.VerifyJSON(fmt.Sprintf(`{"metadata": %s}`, metadata)),
-				),
-			)
-
-			err := rackhdapi.SetNodeMetadata(cpiConfig, nodeID, metadata)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(server.ReceivedRequests()).To(HaveLen(1))
-		})
-	})
+      err := rackhdapi.SetNodeMetadata(cpiConfig, nodeID, metadata)
+      Expect(err).ToNot(HaveOccurred())
+      Expect(server.ReceivedRequests()).To(HaveLen(1))
+    })
+  })
 })
