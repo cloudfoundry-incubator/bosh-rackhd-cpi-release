55cbbbe3fccf2a98acd4d061543650774e306e73
Xuebin He <xuebin.he@emc.com>
2016-12-07 14:22:04 -0500

    Update set_vm_metadate for 2.0
    Signed-off-by: Ryan Murphy <ryan.p.murphy@emc.com>

--- src/github.com/rackhd/rackhd-cpi/cpi/set_vm_metadata_test.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/set_vm_metadata_test.go
@@ -1,65 +1,59 @@
 package cpi_test
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"strings"
+  "fmt"
+  "net/http"
+  "strings"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/ghttp"
-	"github.com/rackhd/rackhd-cpi/bosh"
-	"github.com/rackhd/rackhd-cpi/config"
-	"github.com/rackhd/rackhd-cpi/cpi"
-	"github.com/rackhd/rackhd-cpi/helpers"
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  "github.com/onsi/gomega/ghttp"
+  "github.com/rackhd/rackhd-cpi/bosh"
+  "github.com/rackhd/rackhd-cpi/config"
+  "github.com/rackhd/rackhd-cpi/cpi"
+  "github.com/rackhd/rackhd-cpi/helpers"
 )
 
 var _ = Describe("Setting VM Metadata", func() {
-	Context("When called with metadata", func() {
-		var server *ghttp.Server
-		var jsonReader *strings.Reader
-		var cpiConfig config.Cpi
-		var request bosh.CpiRequest
+  Context("When called with metadata", func() {
+   var server *ghttp.Server
+   var jsonReader *strings.Reader
+   var cpiConfig config.Cpi
+   var request bosh.CpiRequest
 
-		BeforeEach(func() {
-			server, jsonReader, cpiConfig, request = helpers.SetUp(bosh.SET_VM_METADATA)
-		})
+   BeforeEach(func() {
+     server, jsonReader, cpiConfig, request = helpers.SetUp(bosh.SET_VM_METADATA)
+   })
 
-		AfterEach(func() {
-			server.Close()
-		})
+   AfterEach(func() {
+     server.Close()
+   })
 
-		It("Sends a request to set metadata to the RackHD API", func() {
+   It("Sends a request to set metadata to the RackHD API", func() {
+     id := "57fb9fb03fcc55c807add41c"
+     cid := "vm-5678"
+     metadata := map[string]interface{}{
+       "stuff":  "definitely",
+       "thing1": 3563456,
+       "thing2": "bloop",
+     }
 
-			id := "55e79ea54e66816f6152fff9"
-			cid := "vm-5678"
-			metadata := map[string]interface{}{
-				"stuff":  "definitely",
-				"thing1": 3563456,
-				"thing2": "bloop",
-			}
+     var metadataInput bosh.MethodArguments
+     metadataInput = append(metadataInput, cid)
+     metadataInput = append(metadataInput, metadata)
+     expectedNodesBytes := helpers.LoadJSON("../spec_assets/tag_node_with_cid.json")
 
-			var metadataInput bosh.MethodArguments
-			metadataInput = append(metadataInput, cid)
-			metadataInput = append(metadataInput, metadata)
+     server.AppendHandlers(
+       ghttp.CombineHandlers(
+         ghttp.VerifyRequest("GET", "/api/2.0/tags/"+cid+"/nodes"),
+         ghttp.RespondWith(http.StatusOK, expectedNodesBytes),
+       ),
+       ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", id)),
+     )
 
-			expectedNodes := helpers.LoadNodes("../spec_assets/dummy_all_nodes_are_vms.json")
-			expectedNodesData, err := json.Marshal(expectedNodes)
-			Expect(err).ToNot(HaveOccurred())
-
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/2.0/nodes"),
-					ghttp.RespondWith(http.StatusOK, expectedNodesData),
-				),
-				ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", id)),
-			)
-
-			err = cpi.SetVMMetadata(cpiConfig, metadataInput)
-			Expect(err).ToNot(HaveOccurred())
-
-			Expect(server.ReceivedRequests()).To(HaveLen(2))
-		})
-	})
+     err := cpi.SetVMMetadata(cpiConfig, metadataInput)
+     Expect(err).ToNot(HaveOccurred())
+     Expect(server.ReceivedRequests()).To(HaveLen(2))
+   })
+  })
 })
