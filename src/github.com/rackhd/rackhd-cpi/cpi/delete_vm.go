00bda84d69d411d23415a44b6629cde4973ad78d
Ryan Murphy <ryan.p.murphy@emc.com>
2016-12-07 12:12:32 -0500

    Update delete_vm to 2.0
    Also:
    - Delete duplicate definition for deprovision workflows tasks and graphs
    - comment tests for persistent disk, should uncomment when disk part is
    done
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- src/github.com/rackhd/rackhd-cpi/cpi/delete_vm.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/delete_vm.go
@@ -1,50 +1,50 @@
 package cpi
 
 import (
-	"errors"
-	"reflect"
+  "errors"
+  "reflect"
 
-	"github.com/rackhd/rackhd-cpi/bosh"
-	"github.com/rackhd/rackhd-cpi/config"
-	"github.com/rackhd/rackhd-cpi/rackhdapi"
-	"github.com/rackhd/rackhd-cpi/workflows"
+ 	"github.com/rackhd/rackhd-cpi/bosh"
+ 	"github.com/rackhd/rackhd-cpi/config"
+ 	"github.com/rackhd/rackhd-cpi/rackhdapi"
+ 	"github.com/rackhd/rackhd-cpi/workflows"
 )
 
 func DeleteVM(c config.Cpi, extInput bosh.MethodArguments) error {
-	var cid string
-	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(cid) {
-		return errors.New("Received unexpected type for vm cid")
-	}
+ 	var cid string
+ 	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(cid) {
+ 		return errors.New("Received unexpected type for vm cid")
+ 	}
 
-	cid = extInput[0].(string)
-	node, err := rackhdapi.GetNodeByVMCID(c, cid)
+ 	cid = extInput[0].(string)
+ 	node, err := rackhdapi.GetNodeByVMCID(c, cid)
+ 	if err != nil {
+ 		return err
+ 	}
+
+ 	//if node.PersistentDisk.IsAttached {
+ 	//	err = rackhdapi.MakeDiskRequest(c, node, false)
+ 	//	if err != nil {
+ 	//		return err
+ 	//	}
+ 	//}
+
+ 	workflowName, err := workflows.PublishDeprovisionNodeWorkflow(c)
+ 	if err != nil {
+ 		return err
+ 	}
+
+ 	err = workflows.RunDeprovisionNodeWorkflow(c, node.ID, workflowName)
+ 	if err != nil {
+ 		return err
+ 	}
+
+ 	//if node.PersistentDisk.DiskCID == "" {
+	err = rackhdapi.ReleaseNode(c, node.ID)
 	if err != nil {
-		return err
-	}
+ 		return err
+ 	}
+ 	//}
 
-	if node.PersistentDisk.IsAttached {
-		err = rackhdapi.MakeDiskRequest(c, node, false)
-		if err != nil {
-			return err
-		}
-	}
-
-	workflowName, err := workflows.PublishDeprovisionNodeWorkflow(c)
-	if err != nil {
-		return err
-	}
-
-	err = workflows.RunDeprovisionNodeWorkflow(c, node.ID, workflowName)
-	if err != nil {
-		return err
-	}
-
-	if node.PersistentDisk.DiskCID == "" {
-		err = rackhdapi.ReleaseNode(c, node.ID)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+ 	return nil
 }
