56252d129f3ccfbefe434b4dc9b043add07727b4
Xuebin He <xuebin.he@emc.com>
2016-12-08 17:09:57 -0500

    Update delete_disk
    Signed-off-by: Yuval Kohavi <yuval.kohavi@emc.com>

--- src/github.com/rackhd/rackhd-cpi/cpi/delete_disk.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/delete_disk.go
@@ -1,55 +1,63 @@
 package cpi
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"reflect"
+  "encoding/json"
+  "errors"
+  "fmt"
+  "reflect"
+  "strings"
 
-	"github.com/rackhd/rackhd-cpi/bosh"
-	"github.com/rackhd/rackhd-cpi/config"
-	"github.com/rackhd/rackhd-cpi/rackhdapi"
+  "github.com/rackhd/rackhd-cpi/bosh"
+  "github.com/rackhd/rackhd-cpi/config"
+  "github.com/rackhd/rackhd-cpi/models"
+  "github.com/rackhd/rackhd-cpi/rackhdapi"
 )
 
+// DeleteDisk deprovisions disk
 func DeleteDisk(c config.Cpi, extInput bosh.MethodArguments) error {
-	var diskCID string
+  var diskCID string
 
-	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(diskCID) {
-		return errors.New("Received unexpected type for disk cid")
-	}
+  if reflect.TypeOf(extInput[0]) != reflect.TypeOf(diskCID) {
+    return errors.New("Received unexpected type for disk cid")
+  }
+  diskCID = extInput[0].(string)
 
-	diskCID = extInput[0].(string)
+  node, err := rackhdapi.GetNodeByTag(c, diskCID)
 
-	nodes, err := rackhdapi.GetNodes(c)
-	if err != nil {
-		return err
-	}
+  if err != nil {
+    return err
+  }
+  if node.PersistentDisk.IsAttached {
+    return fmt.Errorf("disk: %s is attached", diskCID)
+  }
 
-	for _, node := range nodes {
-		if node.PersistentDisk.DiskCID == diskCID {
-			if node.PersistentDisk.IsAttached {
-				return fmt.Errorf("Disk: %s is attached\n", diskCID)
-			}
+  container := models.PersistentDiskSettingsContainer{
+    PersistentDisk: models.PersistentDiskSettings{},
+  }
+  bodyBytes, err := json.Marshal(container)
+  if err != nil {
+    return err
+  }
 
-			container := rackhdapi.PersistentDiskSettingsContainer{
-				PersistentDisk: rackhdapi.PersistentDiskSettings{},
-			}
-			bodyBytes, err := json.Marshal(container)
-			if err != nil {
-				return err
-			}
-			rackhdapi.PatchNode(c, node.ID, bodyBytes)
+  err = rackhdapi.PatchNode(c, node.ID, bodyBytes)
+  if err != nil {
+    return fmt.Errorf("error deleting disk metadata %s: %s", diskCID, err)
+  }
 
-			if node.CID == "" {
-				err = rackhdapi.ReleaseNode(c, node.ID)
-				if err != nil {
-					fmt.Errorf("error releasing node after delete disk %s: %v", diskCID, err)
-				}
-			}
+  err = rackhdapi.DeleteTag(c, node.ID, diskCID)
+  if err != nil {
+    return fmt.Errorf("error deleting disk cid tag %s: %s", diskCID, err)
+  }
 
-			return nil
-		}
-	}
+  for _, tag := range node.Tags {
+    if strings.HasPrefix(tag, VMCIDTagPrefix) {
+      return nil
+    }
+  }
 
-	return fmt.Errorf("Disk: %s not found\n", diskCID)
+  err = rackhdapi.ReleaseNode(c, node.ID)
+  if err != nil {
+    return fmt.Errorf("error releasing node after delete disk %s: %v", diskCID, err)
+  }
+  return nil
 }
