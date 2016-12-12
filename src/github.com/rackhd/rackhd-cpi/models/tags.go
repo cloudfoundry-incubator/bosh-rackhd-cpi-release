999c9a3888968174b627329b10f147a50f5b91e0
Megan Murawski <megan.hyland@emc.com>
2016-12-12 15:00:15 -0500

    Add unavailable status to tags
    Signed-off-by: Xuebin He <xuebin.he@emc.com>
    Signed-off-by: Megan Murawski <megan.hyland@emc.com>

--- src/github.com/rackhd/rackhd-cpi/models/tags.go
+++ src/github.com/rackhd/rackhd-cpi/models/tags.go
@@ -1,19 +1,33 @@
 package models
 
+// status of nodes that will be taged
 const (
-	Blocked        = "blocked"
-	Reserved       = "reserved"
-	Available      = "available"
-	PersistentDisk = "diskprovisioned"
+  Blocked        = "blocked"
+  Unavailable    = "unavailable"
 )
 
 // Tags encapsulates a JSON of "tags" array for requests
 type Tags struct {
-	T []string `json:"tags"`
+  T []string `json:"tags"`
 }
 
 // TagNode is a node with and ID and an array of tags
 type TagNode struct {
-	ID   string   `json:"id"`
-	Tags []string `json:"tags"`
+  ID   string   `json:"id"`
+  Tags []string `json:"tags"`
+  PersistentDisk PersistentDiskSettings `json:"persistent_disk"`
 }
+
+// PersistentDiskSettingsContainer is used to extract persistent_disk from tagnode
+type PersistentDiskSettingsContainer struct {
+  PersistentDisk PersistentDiskSettings `json:"persistent_disk"`
+}
+
+// PersistentDiskSettings is used to store value for persistent_disk
+type PersistentDiskSettings struct {
+  PregeneratedDiskCID string `json:"pregenerated_disk_cid"`
+  DiskCID             string `json:"disk_cid"`
+  Location            string `json:"location"`
+  IsAttached          bool   `json:"attached"`
+}
+
