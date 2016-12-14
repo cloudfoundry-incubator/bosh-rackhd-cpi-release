18cf847fdf68adff27e208252ec5331d494c56fc
Xuebin He <xuebin.he@emc.com>
2016-12-14 10:51:24 -0500

    Add delete disk functionality to delete_vm
    Also:
    - Update spec assets

--- src/github.com/rackhd/rackhd-cpi/cpi/delete_vm.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/delete_vm.go
@@ -1,50 +1,52 @@
 package cpi
 
 import (
-  "errors"
-  "reflect"
+	"errors"
+	"reflect"
+	"strings"
 
- 	"github.com/rackhd/rackhd-cpi/bosh"
- 	"github.com/rackhd/rackhd-cpi/config"
- 	"github.com/rackhd/rackhd-cpi/rackhdapi"
- 	"github.com/rackhd/rackhd-cpi/workflows"
+	"github.com/rackhd/rackhd-cpi/bosh"
+	"github.com/rackhd/rackhd-cpi/config"
+	"github.com/rackhd/rackhd-cpi/rackhdapi"
+	"github.com/rackhd/rackhd-cpi/workflows"
 )
 
+// DeleteVM deprovision a vm
 func DeleteVM(c config.Cpi, extInput bosh.MethodArguments) error {
- 	var cid string
- 	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(cid) {
- 		return errors.New("Received unexpected type for vm cid")
- 	}
+	var cid string
+	if reflect.TypeOf(extInput[0]) != reflect.TypeOf(cid) {
+		return errors.New("Received unexpected type for vm cid")
+	}
 
- 	cid = extInput[0].(string)
- 	node, err := rackhdapi.GetNodeByVMCID(c, cid)
- 	if err != nil {
- 		return err
- 	}
-
- 	//if node.PersistentDisk.IsAttached {
- 	//	err = rackhdapi.MakeDiskRequest(c, node, false)
- 	//	if err != nil {
- 	//		return err
- 	//	}
- 	//}
-
- 	workflowName, err := workflows.PublishDeprovisionNodeWorkflow(c)
- 	if err != nil {
- 		return err
- 	}
-
- 	err = workflows.RunDeprovisionNodeWorkflow(c, node.ID, workflowName)
- 	if err != nil {
- 		return err
- 	}
-
- 	//if node.PersistentDisk.DiskCID == "" {
-	err = rackhdapi.ReleaseNode(c, node.ID)
+	cid = extInput[0].(string)
+	node, err := rackhdapi.GetNodeByVMCID(c, cid)
 	if err != nil {
- 		return err
- 	}
- 	//}
+		return err
+	}
 
- 	return nil
+	if node.PersistentDisk.IsAttached {
+		err = rackhdapi.MakeDiskRequest(c, node, false)
+		if err != nil {
+			return err
+		}
+	}
+
+	workflowName, err := workflows.PublishDeprovisionNodeWorkflow(c)
+	if err != nil {
+		return err
+	}
+
+	err = workflows.RunDeprovisionNodeWorkflow(c, node.ID, workflowName)
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range node.Tags {
+		if strings.HasPrefix(tag, DiskCIDTagPrefix) {
+			return nil
+		}
+	}
+
+	err = rackhdapi.ReleaseNode(c, node.ID)
+	return err
 }
--- src/github.com/rackhd/rackhd-cpi/cpi/delete_vm_test.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/delete_vm_test.go
@@ -1,135 +1,149 @@
 package cpi_test
 
 import (
- 	"encoding/json"
- 	"net/http"
- 	"strings"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
 
- 	. "github.com/onsi/ginkgo"
- 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+	"github.com/rackhd/rackhd-cpi/bosh"
+	"github.com/rackhd/rackhd-cpi/config"
+	"github.com/rackhd/rackhd-cpi/cpi"
+	"github.com/rackhd/rackhd-cpi/helpers"
+	"github.com/rackhd/rackhd-cpi/models"
 
- 	"github.com/onsi/gomega/ghttp"
- 	"github.com/rackhd/rackhd-cpi/bosh"
- 	"github.com/rackhd/rackhd-cpi/config"
- 	"github.com/rackhd/rackhd-cpi/cpi"
- 	"github.com/rackhd/rackhd-cpi/helpers"
-  "github.com/rackhd/rackhd-cpi/models"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("DeleteVM", func() {
- 	var server *ghttp.Server
- 	var jsonReader *strings.Reader
- 	var cpiConfig config.Cpi
- 	var request bosh.CpiRequest
+	var server *ghttp.Server
+	var jsonReader *strings.Reader
+	var cpiConfig config.Cpi
+	var request bosh.CpiRequest
 
- 	BeforeEach(func() {
- 		server, jsonReader, cpiConfig, request = helpers.SetUp(bosh.DELETE_VM)
- 		cpiConfig.RequestID = "requestid"
- 	})
+	BeforeEach(func() {
+		server, jsonReader, cpiConfig, request = helpers.SetUp(bosh.DELETE_VM)
+		cpiConfig.RequestID = "requestid"
+	})
 
- 	AfterEach(func() {
- 		server.Close()
- 	})
+	AfterEach(func() {
+		server.Close()
+	})
 
- 	Context("with a valid VM CID and valid states", func() {
- 		var extInput bosh.MethodArguments
+	Context("with a valid VM CID and valid states", func() {
+		var extInput bosh.MethodArguments
 
- 		//Context("when there is a persistent disk left before deprovisioning", func() {
- 		//	It("deprovisions the node", func() {
-      //  expectedNodesData := helpers.LoadJSON("../spec_assets/tag_node_with_cid.json")
-      //  server.AppendHandlers(
-      //    ghttp.CombineHandlers(
-      //      ghttp.VerifyRequest("GET", "/api/2.0/tags/vm-5678/nodes"),
-      //      ghttp.RespondWith(http.StatusOK, expectedNodesData),
-      //    ),
-      //  )
-     //
-     //
- 		//		jsonInput := []byte(`["vm-5678"]`)
- 		//		err := json.Unmarshal(jsonInput, &extInput)
- 		//		Expect(err).NotTo(HaveOccurred())
-     //
- 		//		server.AppendHandlers(
- 		//			helpers.MakeWorkflowHandlers(
- 		//				"Deprovision",
- 		//				cpiConfig.RequestID,
- 		//				"57fb9fb03fcc55c807add41c",
- 		//			)...,
- 		//		)
-     //
- 		//		err = cpi.DeleteVM(cpiConfig, extInput)
- 		//		Expect(err).NotTo(HaveOccurred())
- 		//		Expect(len(server.ReceivedRequests())).To(Equal(8))
- 		//	})
- 		//})
+		Context("when there is a persistent disk left before deprovisioning", func() {
+			It("deprovisions the node", func() {
+				vmCID := "vm_cid-fake_uuid"
+				expectedNodesData := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_disk_detached.json")
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+						ghttp.RespondWith(http.StatusOK, expectedNodesData),
+					),
+				)
 
- 		Context("when there is no persistent disk left before deprovisioning", func() {
- 			It("deprovisions the node and sets the status to available", func() {
-        expectedNodesData := helpers.LoadJSON("../spec_assets/tag_node_with_cid.json")
-        server.AppendHandlers(
-          ghttp.CombineHandlers(
-            ghttp.VerifyRequest("GET", "/api/2.0/tags/vm-1234/nodes"),
-            ghttp.RespondWith(http.StatusOK, expectedNodesData),
-          ),
-        )
+				jsonInput := []byte(`["` + vmCID + `"]`)
+				err := json.Unmarshal(jsonInput, &extInput)
+				Expect(err).NotTo(HaveOccurred())
 
- 				jsonInput := []byte(`["vm-1234"]`)
- 				err := json.Unmarshal(jsonInput, &extInput)
- 				Expect(err).NotTo(HaveOccurred())
+				server.AppendHandlers(
+					helpers.MakeWorkflowHandlers(
+						"Deprovision",
+						cpiConfig.RequestID,
+						"57fb9fb03fcc55c807add41c",
+					)...,
+				)
 
- 				server.AppendHandlers(
- 					helpers.MakeWorkflowHandlers(
- 						"Deprovision",
- 						cpiConfig.RequestID,
- 						"57fb9fb03fcc55c807add41c",
- 					)...,
- 				)
- 				server.AppendHandlers(
- 					ghttp.CombineHandlers(
- 						ghttp.VerifyRequest("DELETE", "/api/2.0/nodes/57fb9fb03fcc55c807add41c/tags/"+models.Unavailable),
- 					  ghttp.RespondWith(http.StatusNoContent, nil),
-          ),
- 				)
+				err = cpi.DeleteVM(cpiConfig, extInput)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(server.ReceivedRequests())).To(Equal(8))
+			})
+		})
 
- 				err = cpi.DeleteVM(cpiConfig, extInput)
- 				Expect(err).NotTo(HaveOccurred())
- 				Expect(len(server.ReceivedRequests())).To(Equal(9))
- 			})
- 		})
+		Context("when there is no persistent disk left before deprovisioning", func() {
+			It("deprovisions the node and sets the status to available", func() {
+				expectedNodesData := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_cid.json")
+				vmCID := "vm-1234"
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+						ghttp.RespondWith(http.StatusOK, expectedNodesData),
+					),
+				)
 
- 		//Context("when there are attached disks to a VM", func() {
- 		//	It("detaches a disk from the VM and deletes the VM", func() {
- 		//		jsonInput := []byte(`["valid_vm_cid_2"]`)
- 		//		err := json.Unmarshal(jsonInput, &extInput)
- 		//		Expect(err).NotTo(HaveOccurred())
- 		//		nodeID := "5665a65a0561790005b77b85"
- 		//		container := rackhdapi.PersistentDiskSettingsContainer{
- 		//			PersistentDisk: rackhdapi.PersistentDiskSettings{
- 		//				DiskCID:    fmt.Sprintf("%s-%s", nodeID, cpiConfig.RequestID),
- 		//				Location:   fmt.Sprintf("/dev/%s", rackhdapi.PersistentDiskLocation),
- 		//				IsAttached: false,
- 		//			},
- 		//		}
- 		//		expectedPersistentDiskSettings, err := json.Marshal(container)
- 		//		Expect(err).ToNot(HaveOccurred())
-    //
- 		//		server.AppendHandlers(
- 		//			ghttp.CombineHandlers(
- 		//				ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
- 		//				ghttp.VerifyJSON(string(expectedPersistentDiskSettings)),
- 		//			),
- 		//		)
- 		//		server.AppendHandlers(
- 		//			helpers.MakeWorkflowHandlers(
- 		//				"Deprovision",
- 		//				cpiConfig.RequestID,
- 		//				nodeID,
- 		//			)...,
- 		//		)
- 		//		err = cpi.DeleteVM(cpiConfig, extInput)
- 		//		Expect(err).NotTo(HaveOccurred())
- 		//		Expect(len(server.ReceivedRequests())).To(Equal(9))
- 		//	})
- 		//})
- 	})
+				jsonInput := []byte(`["` + vmCID + `"]`)
+				err := json.Unmarshal(jsonInput, &extInput)
+				Expect(err).NotTo(HaveOccurred())
+
+				server.AppendHandlers(
+					helpers.MakeWorkflowHandlers(
+						"Deprovision",
+						cpiConfig.RequestID,
+						"57fb9fb03fcc55c807add41c",
+					)...,
+				)
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("DELETE", "/api/2.0/nodes/57fb9fb03fcc55c807add41c/tags/"+models.Unavailable),
+						ghttp.RespondWith(http.StatusNoContent, nil),
+					),
+				)
+
+				err = cpi.DeleteVM(cpiConfig, extInput)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(server.ReceivedRequests())).To(Equal(9))
+			})
+		})
+
+		Context("when there are attached disks to a VM", func() {
+			It("detaches a disk from the VM and deletes the VM", func() {
+				vmCID := "vm_cid-fake_uuid"
+
+				jsonInput := []byte(`["` + vmCID + `"]`)
+				err := json.Unmarshal(jsonInput, &extInput)
+				Expect(err).NotTo(HaveOccurred())
+
+				expectedNodesData := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_disk_attached.json")
+				var expectedNodes []models.TagNode
+				err = json.Unmarshal(expectedNodesData, &expectedNodes)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(len(expectedNodes)).To(Equal(1))
+
+				expectedNode := expectedNodes[0]
+				expectedNode.PersistentDisk.IsAttached = false
+				nodeID := expectedNode.ID
+
+				expectedPersistentDiskSettings, err := json.Marshal(map[string]interface{}{
+					"persistent_disk": expectedNode.PersistentDisk,
+				})
+				Expect(err).ToNot(HaveOccurred())
+
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+						ghttp.RespondWith(http.StatusOK, expectedNodesData),
+					),
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", nodeID)),
+						ghttp.VerifyJSON(string(expectedPersistentDiskSettings)),
+						ghttp.RespondWith(http.StatusOK, nil),
+					),
+				)
+				server.AppendHandlers(
+					helpers.MakeWorkflowHandlers(
+						"Deprovision",
+						cpiConfig.RequestID,
+						nodeID,
+					)...,
+				)
+				err = cpi.DeleteVM(cpiConfig, extInput)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(len(server.ReceivedRequests())).To(Equal(9))
+			})
+		})
+	})
 })
