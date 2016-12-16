274ba841e62824e1b072142fd2da4294f846008b
Xuebin He <xuebin.he@emc.com>
2016-12-16 10:04:30 -0500

    Update detach_disk

--- src/github.com/rackhd/rackhd-cpi/cpi/detach_disk.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/detach_disk.go
@@ -4,12 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/rackhd/rackhd-cpi/bosh"
 	"github.com/rackhd/rackhd-cpi/config"
 	"github.com/rackhd/rackhd-cpi/rackhdapi"
 )
 
+// DetachDisk detaches disk from vm
 func DetachDisk(c config.Cpi, extInput bosh.MethodArguments) error {
 	var vmCID string
 	var diskCID string
@@ -25,24 +27,24 @@ func DetachDisk(c config.Cpi, extInput bosh.MethodArguments) error {
 	vmCID = extInput[0].(string)
 	diskCID = extInput[1].(string)
 
-	nodes, err := rackhdapi.GetNodes(c)
+	node, err := rackhdapi.GetNodeByVMCID(c, vmCID)
 	if err != nil {
 		return err
 	}
 
-	for _, node := range nodes {
-		if node.PersistentDisk.DiskCID == diskCID {
-			if !node.PersistentDisk.IsAttached {
-				return fmt.Errorf("Disk: %s is detached\n", diskCID)
+	for _, tag := range node.Tags {
+		if strings.HasPrefix(tag, DiskCIDTagPrefix) {
+			if tag != diskCID {
+				return fmt.Errorf("another disk is attached to VM %s", vmCID)
 			}
 
-			if node.CID != vmCID {
-				return fmt.Errorf("Disk %s does not belong to VM %s\n", diskCID, vmCID)
+			if !node.PersistentDisk.IsAttached {
+				return fmt.Errorf("disk: %s is already detached to VM %s", diskCID, vmCID)
 			}
 
 			return rackhdapi.MakeDiskRequest(c, node, false)
 		}
 	}
 
-	return fmt.Errorf("Disk: %s not found\n", diskCID)
+	return fmt.Errorf("disk: %s was not found on VM %s", diskCID, vmCID)
 }
--- src/github.com/rackhd/rackhd-cpi/cpi/detach_disk_test.go
+++ src/github.com/rackhd/rackhd-cpi/cpi/detach_disk_test.go
@@ -2,18 +2,19 @@ package cpi_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/rackhd/rackhd-cpi/bosh"
 	"github.com/rackhd/rackhd-cpi/config"
-	. "github.com/rackhd/rackhd-cpi/cpi"
-	"github.com/rackhd/rackhd-cpi/rackhdapi"
+	"github.com/rackhd/rackhd-cpi/cpi"
+	"github.com/rackhd/rackhd-cpi/helpers"
+	"github.com/rackhd/rackhd-cpi/models"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
-	"github.com/rackhd/rackhd-cpi/helpers"
 )
 
 var _ = Describe("DetachDisk", func() {
@@ -33,26 +34,27 @@ var _ = Describe("DetachDisk", func() {
 	Context("given a disk CID that exists", func() {
 		Context("given a disk CID that is not attached", func() {
 			It("returns an error", func() {
+				vmCID := "vm_cid-fake_uuid"
+				diskCID := "disk_cid-fake_uuid"
 				jsonInput := []byte(`[
-						"valid_vm_cid_1",
-						"valid_disk_cid_1"
-					]`)
+            "` + vmCID + `",
+            "` + diskCID + `"
+          ]`)
 				var extInput bosh.MethodArguments
 				err := json.Unmarshal(jsonInput, &extInput)
 				Expect(err).ToNot(HaveOccurred())
 
-				expectedNodes := helpers.LoadNodes("../spec_assets/dummy_attached_disk_response.json")
-				expectedNodesData, err := json.Marshal(expectedNodes)
-				Expect(err).ToNot(HaveOccurred())
+				expectedNodesBytes := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_disk_detached.json")
 				server.AppendHandlers(
 					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("GET", "/api/2.0/nodes"),
-						ghttp.RespondWith(http.StatusOK, expectedNodesData),
+						ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+						ghttp.RespondWith(http.StatusOK, expectedNodesBytes),
 					),
 				)
 
-				err = DetachDisk(cpiConfig, extInput)
-				Expect(err).To(MatchError("Disk: valid_disk_cid_1 is detached\n"))
+				err = cpi.DetachDisk(cpiConfig, extInput)
+				errMsg := fmt.Sprintf("disk: %s is already detached to VM %s", diskCID, vmCID)
+				Expect(err).To(MatchError(errMsg))
 				Expect(len(server.ReceivedRequests())).To(Equal(1))
 			})
 		})
@@ -60,67 +62,70 @@ var _ = Describe("DetachDisk", func() {
 		Context("given a disk that is attached", func() {
 			Context("when given a vm cid that the disk does not belong to", func() {
 				It("returns an error", func() {
+					vmCID := "vm_cid-fake_uuid"
+					diskCID := "disk_cid-not_exist"
 					jsonInput := []byte(`[
-							"invalid_vm_cid_2",
-							"valid_disk_cid_2"
-						]`)
+            	"` + vmCID + `",
+            	"` + diskCID + `"
+          	]`)
 					var extInput bosh.MethodArguments
 					err := json.Unmarshal(jsonInput, &extInput)
 					Expect(err).ToNot(HaveOccurred())
 
-					expectedNodes := helpers.LoadNodes("../spec_assets/dummy_attached_disk_response.json")
-					expectedNodesData, err := json.Marshal(expectedNodes)
-					Expect(err).ToNot(HaveOccurred())
+					expectedNodesBytes := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_disk_attached.json")
 					server.AppendHandlers(
 						ghttp.CombineHandlers(
-							ghttp.VerifyRequest("GET", "/api/2.0/nodes"),
-							ghttp.RespondWith(http.StatusOK, expectedNodesData),
+							ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+							ghttp.RespondWith(http.StatusOK, expectedNodesBytes),
 						),
 					)
 
-					err = DetachDisk(cpiConfig, extInput)
-					Expect(err).To(MatchError("Disk valid_disk_cid_2 does not belong to VM invalid_vm_cid_2\n"))
+					err = cpi.DetachDisk(cpiConfig, extInput)
+					errMsg := fmt.Sprintf("another disk is attached to VM %s", vmCID)
+					Expect(err).To(MatchError(errMsg))
 					Expect(len(server.ReceivedRequests())).To(Equal(1))
 				})
 			})
 
 			Context("given a VM CID that the disk belongs to", func() {
 				It("detaches the disk", func() {
+					vmCID := "vm_cid-fake_uuid"
+					diskCID := "disk_cid-fake_uuid"
 					jsonInput := []byte(`[
-						"valid_vm_cid_2",
-						"valid_disk_cid_2"
-					]`)
+            	"` + vmCID + `",
+            	"` + diskCID + `"
+          	]`)
 					var extInput bosh.MethodArguments
 					err := json.Unmarshal(jsonInput, &extInput)
-					Expect(err).NotTo(HaveOccurred())
-
-					expectedNodes := helpers.LoadNodes("../spec_assets/dummy_attached_disk_response.json")
-					expectedNodesData, err := json.Marshal(expectedNodes)
 					Expect(err).ToNot(HaveOccurred())
 
-					body := rackhdapi.PersistentDiskSettingsContainer{
-						PersistentDisk: rackhdapi.PersistentDiskSettings{
-							DiskCID:    "valid_disk_cid_2",
-							Location:   "/dev/sdb",
-							IsAttached: false,
-						},
-					}
-					bodyBytes, err := json.Marshal(body)
+					expectedNodesBytes := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_disk_attached.json")
+					var expectedNodes []models.TagNode
+					err = json.Unmarshal(expectedNodesBytes, &expectedNodes)
+					Expect(err).ToNot(HaveOccurred())
+					Expect(len(expectedNodes)).To(Equal(1))
+
+					node := expectedNodes[0]
+					node.PersistentDisk.IsAttached = false
+					expectedPersistanceSettingsBytes, err := json.Marshal(map[string]interface{}{
+						"persistent_disk": node.PersistentDisk,
+					})
 
 					server.AppendHandlers(
 						ghttp.CombineHandlers(
-							ghttp.VerifyRequest("GET", "/api/2.0/nodes"),
-							ghttp.RespondWith(http.StatusOK, expectedNodesData),
+							ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+							ghttp.RespondWith(http.StatusOK, expectedNodesBytes),
 						),
 						ghttp.CombineHandlers(
-							ghttp.VerifyRequest("PATCH", "/api/2.0/nodes/55e79eb14e66816f6152fffb"),
-							ghttp.VerifyJSON(string(bodyBytes)),
+							ghttp.VerifyRequest("PATCH", fmt.Sprintf("/api/2.0/nodes/%s", node.ID)),
+							ghttp.VerifyJSON(string(expectedPersistanceSettingsBytes)),
+							ghttp.RespondWith(http.StatusOK, nil),
 						),
 					)
 
-					err = DetachDisk(cpiConfig, extInput)
-					Expect(len(server.ReceivedRequests())).To(Equal(2))
+					err = cpi.DetachDisk(cpiConfig, extInput)
 					Expect(err).NotTo(HaveOccurred())
+					Expect(len(server.ReceivedRequests())).To(Equal(2))
 				})
 			})
 		})
@@ -128,26 +133,27 @@ var _ = Describe("DetachDisk", func() {
 
 	Context("given a nonexistent disk CID", func() {
 		It("returns an error", func() {
+			vmCID := "vm_cid-fake_uuid"
+			diskCID := "disk_cid-not_exist"
 			jsonInput := []byte(`[
-					"valid_vm_cid_1",
-					"invalid_disk_cid"
+					"` + vmCID + `",
+          "` + diskCID + `"
 				]`)
 			var extInput bosh.MethodArguments
 			err := json.Unmarshal(jsonInput, &extInput)
 			Expect(err).ToNot(HaveOccurred())
 
-			expectedNodes := helpers.LoadNodes("../spec_assets/dummy_attached_disk_response.json")
-			expectedNodesData, err := json.Marshal(expectedNodes)
-			Expect(err).ToNot(HaveOccurred())
+			expectedNodesBytes := helpers.LoadJSON("../spec_assets/tag_nodes_with_vm_cid.json")
 			server.AppendHandlers(
 				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("GET", "/api/2.0/nodes"),
-					ghttp.RespondWith(http.StatusOK, expectedNodesData),
+					ghttp.VerifyRequest("GET", fmt.Sprintf("/api/2.0/tags/%s/nodes", vmCID)),
+					ghttp.RespondWith(http.StatusOK, expectedNodesBytes),
 				),
 			)
 
-			err = DetachDisk(cpiConfig, extInput)
-			Expect(err).To(MatchError("Disk: invalid_disk_cid not found\n"))
+			err = cpi.DetachDisk(cpiConfig, extInput)
+			errMsg := fmt.Sprintf("disk: %s was not found on VM %s", diskCID, vmCID)
+			Expect(err).To(MatchError(errMsg))
 			Expect(len(server.ReceivedRequests())).To(Equal(1))
 		})
 	})
