f7765240b8b5d27605aae9bbb543402fabc36754
Xuebin He <xuebin.he@emc.com>
2016-12-16 17:00:05 -0500

    Rename graphs and tasks

--- src/github.com/rackhd/rackhd-cpi/workflows/check_environment.go
+++ src/github.com/rackhd/rackhd-cpi/workflows/check_environment.go
@@ -1,14 +1,6 @@
 package workflows
 
-// Required Tasks
-const (
-	BootstrapUbuntuTaskName         string = "Task.Linux.Bootstrap.Ubuntu"
-	BootstrapUbuntuTaskTemplatePath string = "../templates/rackhd_bootstrap_ubuntu_task.json"
-	RebootNodeTaskName              string = "Task.Obm.Node.Reboot"
-	RebootNodeTaskTemplatePath      string = "../templates/rackhd_reboot_task.json"
-	SetPxeRebootTaskName            string = "Task.Obm.Node.PxeBoot"
-	SetPxeRebootTaskTemplatePath    string = "../templates/rackhd_set-pxe-boot_task.json"
-)
+
 
 // GetRequiredTasks returns all required tasks name
 func GetRequiredTasks() map[string]string {
