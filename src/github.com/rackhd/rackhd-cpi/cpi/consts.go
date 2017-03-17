ea861653e0d664326d89c6161d6b0870e9187eae
Akshaya Khare <akshaya.khare@emc.com>
2017-03-17 09:25:54 -0400

    Create version branch
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- src/github.com/rackhd/rackhd-cpi/cpi/consts.go
+++
@@ -1,7 +0,0 @@
-package cpi
-
-// VMCIDTagPrefix is a prefix for vm cid
-const (
-	VMCIDTagPrefix   string = "vm_cid-"
-	DiskCIDTagPrefix string = "disk_cid-"
-)
--- src/github.com/rackhd/rackhd-cpi/models/files.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type FileUploadResponse struct {
-	Name   string `json:"name"`
-	UUID   string `json:"uuid"`
-	Md5    string `json:"md5"`
-	Sha256 string `json:"sha256"`
-}
